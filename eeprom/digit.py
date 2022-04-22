bf01a371bdf978fa9056857e7837948434b4d0f3
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-04-22 12:08:10 +0200

    full lcd

--- eeprom/digit.py
+++ eeprom/digit.py
@@ -1,32 +1,37 @@
+from operator import mod
+
+
 digits=["7e","30","6d","79","33","5b","5f","70","7f","7b"]
+digits+=["7d","1f","4e","3d","4f","47"]
 print("v2.0 raw",end="\n")
 
 data=[]
 
-for v in range(256):
-    data.append(digits[v%10])
-for v in range(256):
-    data.append(digits[(v//10)%10])
-for v in range(256):
-    data.append(digits[(v//100)%10])
-for v in range(256):
-    data.append("00")
-    
-ones=[]
-tens=[]
-hundrets=[]
-sign=[]
-for a in range(0,256):
-    v=(a+128)%256-128
-    ones.append(digits[abs(v)%10])
-    tens.append(digits[(abs(v)//10)%10])
-    hundrets.append(digits[(abs(v)//100)%10])
-    sign.append("00" if v>=0 else "01")
+for modulus in [10,16]:
+    for v in range(256):
+        data.append(digits[v%modulus])
+    for v in range(256):
+        data.append(digits[(v//modulus)%modulus])
+    for v in range(256):
+        data.append(digits[(v//(modulus*modulus))%modulus])
+    for v in range(256):
+        data.append("00")
+        
+    ones=[]
+    tens=[]
+    hundrets=[]
+    sign=[]
+    for a in range(0,256):
+        v=(a+128)%256-128
+        ones.append(digits[abs(v)%modulus])
+        tens.append(digits[(abs(v)//modulus)%modulus])
+        hundrets.append(digits[(abs(v)//(modulus*modulus))%modulus])
+        sign.append("00" if v>=0 else "01")
 
-data.extend(ones)
-data.extend(tens)
-data.extend(hundrets)
-data.extend(sign)
+    data.extend(ones)
+    data.extend(tens)
+    data.extend(hundrets)
+    data.extend(sign)
 
 i=0
 for d in data:
